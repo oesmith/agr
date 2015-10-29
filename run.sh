2f7298cdf7aa0bbe6464c992592d01c33d95ba71
Olly Smith <oliver@oesmith.co.uk>
2015-10-29 12:06:09 +0000

    Add build, database name to run script.

--- run.sh
+++ run.sh
@@ -2,8 +2,34 @@
 
 source keys.sh
 
+build=0
+DATABASE_PATH="dev.db"
+
+while getopts ":bd:" opt
+do
+  case $opt in
+    b)
+      BUILD=1
+      ;;
+    d)
+      DATABASE_PATH=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [[ "$BUILD" -eq "1" ]]
+then
+  echo "Building.. "
+  go build || exit 1
+fi
+
 ./agr \
-  -ldb_address $LDB_ADDRESS \
-  -ldb_token $LDB_TOKEN \
-  -transportapi_app_id $TRANSPORTAPI_APP_ID \
-  -transportapi_api_key $TRANSPORTAPI_API_KEY
+  -database_path "$DATABASE_PATH" \
+  -ldb_address "$LDB_ADDRESS" \
+  -ldb_token "$LDB_TOKEN" \
+  -transportapi_app_id "$TRANSPORTAPI_APP_ID" \
+  -transportapi_api_key "$TRANSPORTAPI_API_KEY"
