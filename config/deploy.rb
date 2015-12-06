86aa378b0ca5de01fb02643c6fab3deb8001d70b
Olly Smith <oliver@oesmith.co.uk>
2015-12-06 22:02:18 +0000

    Ensure old processes are killed after deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,11 +23,11 @@ set :deploy_to, '/home/agr'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('db/production.sqlite3')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'db', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 
 # Default value for default_env is {}
@@ -49,7 +49,7 @@ namespace :deploy do
 
   after :publishing, :restart
   task :restart do
-    invoke 'unicorn:restart'
+    invoke 'unicorn:legacy_restart'
   end
 
 end
