7b4bba2aae926afea181a1d53a4d958592dd926e
Olly Smith <oliver@oesmith.co.uk>
2015-12-06 21:01:22 +0000

    Update unicorn config.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,4 +1,9 @@
-listen 3000
+app_path = File.expand_path("../..", __FILE__)
+
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+pid "#{app_path}/tmp/pids/unicorn.pid"
 preload_app true
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
 timeout 30
 worker_processes 2
