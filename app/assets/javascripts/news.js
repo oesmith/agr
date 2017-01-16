06c294684a3db852e9d7997b12fc55587c54cdd6
Olly Smith <oliver@oesmith.co.uk>
2017-01-16 22:17:22 +0000

    Add key controls.

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -1,2 +1,44 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+(function() {
+
+document.addEventListener('keypress', function(event) {
+  if (event.charCode == 106 /* j */) {
+    var currentArticle = getCurrentArticle();
+    if (currentArticle != null) {
+      var nextArticle = currentArticle.nextSibling;
+      while (nextArticle != null && nextArticle.tagName != 'ARTICLE') {
+        nextArticle = nextArticle.nextSibling;
+      }
+      if (nextArticle != null) {
+        nextArticle.scrollIntoView();
+      }
+    }
+  } else if (event.charCode == 107 /* k */) {
+    var currentArticle = getCurrentArticle();
+    if (currentArticle != null) {
+      var prevArticle = currentArticle.previousSibling;
+      while (prevArticle != null && prevArticle.tagName != 'ARTICLE') {
+        prevArticle = prevArticle.previousSibling;
+      }
+      if (prevArticle != null) {
+        prevArticle.scrollIntoView();
+      }
+    }
+  }
+});
+
+function getCurrentArticle() {
+  var articles = document.getElementsByTagName('article');
+  var scrollPos = document.scrollingElement.scrollTop + 22;
+  for (var i = 0; i < articles.length; i++) {
+    var articleEnd = articles[i].offsetTop + articles[i].offsetHeight;
+    if (scrollPos < articleEnd) {
+      return articles[i];
+    }
+  }
+  return null;
+}
+
+})();
