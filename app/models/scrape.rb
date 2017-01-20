642778ea6e30a4dc6014d2c0bd64b093b645b0b1
Olly Smith <oliver@oesmith.co.uk>
2017-01-20 21:09:00 +0000

    Navigate to previous fetches.

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -2,4 +2,8 @@ class Scrape < ApplicationRecord
   belongs_to :user
   has_many :posts
   enum state: [:pending, :complete, :errors]
+
+  def previous
+    @previous ||= self.class.where("user_id = ? and created_at < ?", user_id, created_at).order(created_at: :desc).first
+  end
 end
