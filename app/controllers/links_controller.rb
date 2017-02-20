b74a2f3101602e269eceff307d26de0d46d8d5dd
Olly Smith <oliver@oesmith.co.uk>
2017-02-20 21:47:20 +0000

    Basic bookmark management.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,13 +1,14 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_action :set_link, only: [:show, :destroy]
 
   # GET /links
   def index
-    @links = Link.all
+    @links = Link.where(user: current_user)
   end
 
   # GET /links/1
   def show
+    redirect_to @link.url
   end
 
   # GET /links/new
@@ -15,27 +16,24 @@ class LinksController < ApplicationController
     @link = Link.new
   end
 
-  # GET /links/1/edit
-  def edit
-  end
-
   # POST /links
   def create
+    begin
+      data = Net::HTTP.get(URI(link_params[:url]))
+    rescue
+      render :new, notice: 'Failed to fetch link'
+    end
     @link = Link.new(link_params)
-
+    begin
+      @link.title = Nokogiri.HTML(data).css('title').first.content
+    rescue
+      # noop
+    end
+    @link.user = current_user
     if @link.save
       redirect_to @link, notice: 'Link was successfully created.'
     else
       render :new
-    end
-  end
-
-  # PATCH/PUT /links/1
-  def update
-    if @link.update(link_params)
-      redirect_to @link, notice: 'Link was successfully updated.'
-    else
-      render :edit
     end
   end
 
@@ -48,11 +46,11 @@ class LinksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
-      @link = Link.find(params[:id])
+      @link = Link.find_by!(id: params[:id], user: current_user)
     end
 
     # Only allow a trusted parameter "white list" through.
     def link_params
-      params.require(:link).permit(:title, :url, :followed_at)
+      params.require(:link).permit(:url)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170220081537) do
     t.string   "title"
     t.string   "url"
     t.datetime "followed_at"
+    t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
