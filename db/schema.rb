7fc6cc3b23fdee6203c44619c140518152bb07ec
Olly Smith <oliver@oesmith.co.uk>
2017-02-20 19:12:26 +0000

    Generate Links app.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120174453) do
+ActiveRecord::Schema.define(version: 20170220081537) do
 
   create_table "feeds", force: :cascade do |t|
     t.string   "url"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170120174453) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.string   "title"
+    t.string   "url"
+    t.datetime "followed_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "posts", force: :cascade do |t|
