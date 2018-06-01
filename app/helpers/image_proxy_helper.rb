e921583843583eff4be5319682cc1b8cd79d3037
Olly Smith <olly.smith@gmail.com>
2018-06-01 21:37:18 +0100

    Proxy images.

---
+++ app/helpers/image_proxy_helper.rb
@@ -0,0 +1,11 @@
+require 'base64'
+require 'openssl'
+
+module ImageProxyHelper
+  def proxy_image(url)
+    sig = Base64.urlsafe_encode64(
+      OpenSSL::HMAC.digest(
+        OpenSSL::Digest.new('sha256'), "ahtah5WaeCheikiphai6Kahf2Coh3jei", url)).strip()
+    "https://olly.xyz/i/s#{sig}/#{url}"
+  end
+end
