5d6c986664b65a78b09bec7cfc4960c1f8b6ac5f
Olly Smith <olly.smith@gmail.com>
2018-07-29 20:50:45 +0100

    When was the schema updated?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126100100) do
+ActiveRecord::Schema.define(version: 2018_01_26_100100) do
 
   create_table "feeds", force: :cascade do |t|
     t.string "url"
