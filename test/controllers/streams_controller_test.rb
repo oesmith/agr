71e7e08d925563101a5f81962a77c1847aa613dc
Olly Smith <olly.smith@gmail.com>
2019-08-27 15:50:06 +0100

    Fix some deprecation warnings.

--- test/controllers/streams_controller_test.rb
+++ test/controllers/streams_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class StreamsControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
+  include Devise::Test::ControllerHelpers
 
   setup do
     sign_in(users(:one))
